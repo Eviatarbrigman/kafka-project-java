2052d25d24266a060cf61aac58fbe84f2b72487c
Eviatar brigman <71985255+Eviatarbrigman@users.noreply.github.com>
2024-05-23 15:51:54 +0300

    add tests1

--- producer/src/main/java/com/eviatar/producer/SpringBootProducerApplication.java
+++ producer/src/main/java/com/eviatar/producer/SpringBootProducerApplication.java
@@ -4,9 +4,11 @@ package com.eviatar.producer;
 import com.eviatar.producer.producer.WikimediaProducer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SpringBootProducerApplication implements CommandLineRunner {
 
     public SpringBootProducerApplication(WikimediaProducer wikimediaProducer) {
